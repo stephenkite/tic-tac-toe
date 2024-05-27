7f124093cded0001f71019652d7126ccdf265ff0
stephenkite <kite1533@gmail.com>
2024-05-27 21:53:42 +0300

    Declaring a Winner.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function Square ({ value, onSquareClick }) {
   )
 }
 
-export default function Board() {
+export function Board() {
   const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
   function handleClick (i) {
-    if (squares[i]){
+    if (squares[i] || calculateWinner(squares)){
       return;
     }
     
@@ -27,8 +27,18 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = 'Winner ' + winner
+  } else {
+    status = 'Next Player ' + (xIsNext ? 'X' : 'O');
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -46,4 +56,37 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+export default function Game () {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*To Do*/}</ol>
+      </div>
+    </div>
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c ] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
