ee9378fee330e62c2004ca0231e3a82ff4582f39
stephenkite <kite1533@gmail.com>
2024-05-30 22:43:31 +0300

    Adding time travel

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function Square ({ value, onSquareClick }) {
   )
 }
 
-export function Board() {
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
+export function Board({ xIsNext, squares, onPlay}) {
 
   function handleClick (i) {
     if (squares[i] || calculateWinner(squares)){
@@ -23,8 +21,7 @@ export function Board() {
     }else{
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -59,10 +56,19 @@ export function Board() {
 }
 
 export default function Game () {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay (nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
         <ol>{/*To Do*/}</ol>
